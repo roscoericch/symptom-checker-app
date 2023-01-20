ced3c59c9bf873083a659919db8fe676866eb721
roscoericch <sikirurazak1@gmail.com>
2023-01-20 08:02:04 -0800

    setup auth with axios interceptors

--- src/components/symptoms/symptoms.jsx
+++ src/components/symptoms/symptoms.jsx
@@ -14,7 +14,6 @@ const Symptoms = () => {
     addSelectedSymptoms,
     deleteSelectedSymptoms,
   } = useContext(DataContext);
-  useStore();
   const [filteredSymptoms, setfilteredSymptoms] = useState([]);
   const [searchSymptoms, setSearchSymptoms] = useState("");
   useEffect(() => {
